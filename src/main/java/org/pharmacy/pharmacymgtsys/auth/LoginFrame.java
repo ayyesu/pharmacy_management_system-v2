705d6fbc216e91c4adb6a54d4bc129cecb7e5368
ayyesu <iamdanielkissiedu@gmail.com>
2024-06-22 14:52:34 +0000

    modified search drug frame

--- src/main/java/org/pharmacy/pharmacymgtsys/auth/LoginFrame.java
+++ src/main/java/org/pharmacy/pharmacymgtsys/auth/LoginFrame.java
@@ -17,7 +17,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.pharmacy.pharmacymgtsys.dao.UserDAO;
@@ -30,35 +32,55 @@ public class LoginFrame extends Application {
     public LoginFrame() {
     }
 
+    @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Pharmacy Management System - Login");
+
+        // Load and style the background image
         Image backgroundImage = new Image("file:src/main/resources/img/Pharmacy.jpg");
         ImageView backgroundImageView = new ImageView(backgroundImage);
-        backgroundImageView.setFitWidth(400.0);
-        backgroundImageView.setFitHeight(400.0);
+        backgroundImageView.setFitWidth(400);
+        backgroundImageView.setFitHeight(400);
+
+        // Create an overlay to lower the contrast of the background image
+        Rectangle overlay = new Rectangle(400, 400);
+        overlay.setFill(Color.rgb(0, 0, 0, 0.5));
+
         StackPane root = new StackPane();
+
+        // Create and style the form
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10.0);
-        grid.setVgap(10.0);
-        grid.setPadding(new Insets(25.0, 25.0, 25.0, 25.0));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
         Label title = new Label("Login to your account");
-        title.setFont(Font.font("Arial", 20.0));
-        title.setTextFill(Color.BLACK);
+        title.setFont(Font.font("Verdana", 24));
+        title.setTextFill(Color.WHITE);
         grid.add(title, 0, 0, 2, 1);
+
         Label usernameLabel = new Label("Username:");
-        usernameLabel.setTextFill(Color.BLACK);
+        usernameLabel.setFont(Font.font("Verdana", 14));
+        usernameLabel.setTextFill(Color.WHITE);
         grid.add(usernameLabel, 0, 1);
+
         this.usernameField = new TextField();
         grid.add(this.usernameField, 1, 1);
+
         Label passwordLabel = new Label("Password:");
-        passwordLabel.setTextFill(Color.BLACK);
+        passwordLabel.setFont(Font.font("Verdana", 14));
+        passwordLabel.setTextFill(Color.WHITE);
         grid.add(passwordLabel, 0, 2);
+
         this.passwordField = new PasswordField();
         grid.add(this.passwordField, 1, 2);
+
         Button loginButton = new Button("Login");
+        loginButton.setFont(Font.font("Verdana", 14));
         grid.add(loginButton, 1, 3);
-        loginButton.setOnAction((e) -> {
+
+        loginButton.setOnAction(e -> {
             String username = this.usernameField.getText();
             String password = this.passwordField.getText();
             UserDAO userDAO = new UserDAO();
@@ -66,25 +88,24 @@ public class LoginFrame extends Application {
             try {
                 Alert alertx;
                 if (userDAO.validateUser(username, password)) {
-                    alertx = new Alert(AlertType.INFORMATION, "Login successful!", new ButtonType[0]);
+                    alertx = new Alert(AlertType.INFORMATION, "Login successful!", ButtonType.OK);
                     alertx.showAndWait();
                     WelcomeScreenFrame welcomeScreen = new WelcomeScreenFrame();
                     welcomeScreen.start(new Stage());
                     primaryStage.close();
                 } else {
-                    alertx = new Alert(AlertType.ERROR, "Invalid username or password.", new ButtonType[0]);
+                    alertx = new Alert(AlertType.ERROR, "Invalid username or password.", ButtonType.OK);
                     alertx.showAndWait();
                 }
             } catch (SQLException var8) {
-                SQLException ex = var8;
-                ex.printStackTrace();
-                Alert alert = new Alert(AlertType.ERROR, "Database error.", new ButtonType[0]);
+                var8.printStackTrace();
+                Alert alert = new Alert(AlertType.ERROR, "Database error.", ButtonType.OK);
                 alert.showAndWait();
             }
-
         });
-        root.getChildren().addAll(new Node[]{backgroundImageView, grid});
-        Scene scene = new Scene(root, 400.0, 400.0);
+
+        root.getChildren().addAll(backgroundImageView, overlay, grid);
+        Scene scene = new Scene(root, 400, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
--- src/main/java/org/pharmacy/pharmacymgtsys/ui/SearchDrugFrame.java
+++ src/main/java/org/pharmacy/pharmacymgtsys/ui/SearchDrugFrame.java
@@ -3,6 +3,7 @@ package org.pharmacy.pharmacymgtsys.ui;
 import java.sql.SQLException;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -14,6 +15,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.pharmacy.pharmacymgtsys.dao.DrugDAO;
 import org.pharmacy.pharmacymgtsys.model.Drug;
@@ -24,50 +27,87 @@ public class SearchDrugFrame extends Application {
     public SearchDrugFrame() {
     }
 
+    @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Search Drug");
-        VBox vbox = new VBox(10.0);
-        vbox.setPadding(new Insets(10.0));
+
+        // Create and style the main container
+        VBox vbox = new VBox(10);
+        vbox.setPadding(new Insets(20));
+        vbox.setStyle("-fx-background-color: #f0f4f7;");
+        vbox.setAlignment(Pos.CENTER);
+
+        // Create and style the grid layout for the form
         GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10.0));
-        grid.setHgap(10.0);
-        grid.setVgap(10.0);
+        grid.setPadding(new Insets(20));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setAlignment(Pos.CENTER);
+
+        // Style the label
         Label nameLabel = new Label("Drug Name:");
-        grid.add(nameLabel, 0, 0);
+        nameLabel.setFont(Font.font("Verdana", 14));
+        nameLabel.setTextFill(Color.web("#333"));
+
+        // Style the text field
         TextField nameField = new TextField();
-        grid.add(nameField, 1, 0);
+        nameField.setFont(Font.font("Verdana", 14));
+        nameField.setPromptText("Enter drug name");
+
+        // Style the button
         Button searchButton = new Button("Search");
-        grid.add(searchButton, 1, 1);
+        searchButton.setFont(Font.font("Verdana", 14));
+        searchButton.setStyle("-fx-background-color: #007bff; -fx-text-fill: white; -fx-padding: 5 10 5 10;");
+
+        // Style the result area
         TextArea resultArea = new TextArea();
         resultArea.setEditable(false);
-        searchButton.setOnAction((e) -> {
+        resultArea.setFont(Font.font("Verdana", 14));
+        resultArea.setPromptText("Search results will appear here");
+        resultArea.setStyle("-fx-control-inner-background: #eef;");
+
+        // Add components to the grid
+        grid.add(nameLabel, 0, 0);
+        grid.add(nameField, 1, 0);
+        grid.add(searchButton, 1, 1);
+
+        // Set button action
+        searchButton.setOnAction(e -> {
             String drugName = nameField.getText();
-
-            Alert alert;
             try {
-                Drug drug = this.drugDAO.getDrugByName(drugName);
+                Drug drug = drugDAO.getDrugByName(drugName);
                 if (drug != null) {
-                    String var10001 = drug.getDrugName();
-                    resultArea.setText("Name: " + var10001 + "\nDescription: " + drug.getDescription() + "\nPrice: " + drug.getPrice() + "\nStock: " + drug.getStock());
+                    resultArea.setText(
+                            "Name: " + drug.getDrugName() + "\n" +
+                                    "Description: " + drug.getDescription() + "\n" +
+                                    "Price: " + drug.getPrice() + "\n" +
+                                    "Stock: " + drug.getStock()
+                    );
                 } else {
-                    alert = new Alert(AlertType.ERROR, "Couldn't retrieve drug, please check drug name for validity", new ButtonType[0]);
-                    alert.showAndWait();
+                    showAlert(AlertType.ERROR, "Drug not found", "Couldn't retrieve drug, please check drug name for validity.");
                 }
-            } catch (SQLException var7) {
-                SQLException exx = var7;
-                alert = new Alert(AlertType.ERROR, "Error searching for drug: " + exx.getMessage(), new ButtonType[0]);
-                alert.showAndWait();
+            } catch (SQLException ex) {
+                showAlert(AlertType.ERROR, "Database Error", "Error searching for drug: " + ex.getMessage());
             }
-
         });
-        vbox.getChildren().addAll(new Node[]{grid, resultArea});
-        Scene scene = new Scene(vbox, 400.0, 300.0);
+
+        // Add grid and result area to the VBox
+        vbox.getChildren().addAll(grid, resultArea);
+
+        // Create and set the scene
+        Scene scene = new Scene(vbox, 500, 400);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void showAlert(AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType, message, ButtonType.OK);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.showAndWait();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
